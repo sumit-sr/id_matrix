ee54bf861fe66b8b9e64eb663b58e7ca02e51598
Andre Mouton <andre@amtek.co.za>
2014-06-17 11:04:04 +0200

    [#501] Add rspec setup

---
+++ spec/spec_helper.rb
@@ -0,0 +1,20 @@
+require 'bundler/setup'
+require 'veda_idmatrix'
+require 'shoulda/matchers'
+Bundler.setup
+
+# ActiveRecord::Base.establish_connection(
+#   :adapter => 'sqlite3',
+#   :database => ':memory:'
+#   )
+# require 'schema'
+
+RSpec.configure do |config|
+  config.run_all_when_everything_filtered = true
+  config.filter_run :focus
+  # Run specs in random order to surface order dependencies. 
+  config.order = 'random'
+  config.expect_with :rspec do |c|
+    c.syntax = :expect
+  end
+end
