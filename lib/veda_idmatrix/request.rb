8222308b7a6774b19e4d5b627a930d404063d357
Sumit <sumit.raghuwanshi192@gmail.com>
2018-02-27 23:51:18 +0530

    Add consents in request

--- lib/veda_idmatrix/request.rb
+++ lib/veda_idmatrix/request.rb
@@ -25,7 +25,7 @@ class VedaIdmatrix::Request < ActiveRecord::Base
   end
 
   def to_xml_body
-    doc = self.to_dom('request', self.id_matrix_operation, {:'client-reference'=>self.enquiry[:client_reference], :'reason-for-enquiry'=>self.enquiry[:reason_for_enquiry]}).to_xml.gsub(/(<[\/]?)/,'\1idm:')
+    doc = self.to_dom('request', self.id_matrix_operation, {:'client-reference'=>self.enquiry[:client_reference], :'enquiry-id' => self.enquiry[:enquiry_id], :'reason-for-enquiry'=>self.enquiry[:reason_for_enquiry]}).to_xml.gsub(/(<[\/]?)/,'\1idm:')
     self.xml = doc.gsub('<idm:?xml version="1.0"?>','')
   end
 
@@ -39,7 +39,17 @@ class VedaIdmatrix::Request < ActiveRecord::Base
       if data.is_a? Hash
         builder.send(node, attrs) do
           data.keys.each do |k|
-            builder << to_dom(k, data[k]).root.to_xml
+            if (k == :consents)
+              builder << to_dom(k, data[k]).root.to_xml
+            elsif (k==:consent)
+                data[k].each do |consent_attr|
+                consent = to_dom(k, consent_attr[:text]).root
+                consent[:status] = consent_attr[:status]
+                builder << consent.to_xml
+              end
+            else
+              builder << to_dom(k, data[k]).root.to_xml
+            end
           end
         end
       else
@@ -93,7 +103,8 @@ class VedaIdmatrix::Request < ActiveRecord::Base
 
     drivers_licence_details = {
       :'state-code' => (self.entity[:drivers_licence_state_code]),
-      :'number' => (self.entity[:drivers_licence_number])
+      :'number' => (self.entity[:drivers_licence_number]),
+      :'name-as-on-document' => (self.entity[:drivers_licence_name])
     }
 
     passport_details = {
@@ -101,9 +112,13 @@ class VedaIdmatrix::Request < ActiveRecord::Base
       :'number' => (self.entity[:passport_number])
     }
 
+    consents = {
+      :consent => self.entity[:consents][:consent]
+    }
+
     # Make sure items generated in order #5519
     details = ActiveSupport::OrderedHash.new
-    # details[:'consents'] = consents
+    details[:consents] = consents
     details[:'individual-name'] = individual_name
     details[:'date-of-birth'] = date_of_birth
     details[:'gender'] = gender
@@ -171,3 +186,98 @@ class VedaIdmatrix::Request < ActiveRecord::Base
   end
 
 end
+
+  # def to_dom(node, data, attrs={})
+  #   doc = Nokogiri::XML::Builder.new do |builder|
+  #     if data.is_a?(Hash) && data.keys.sort == [:attributes, :value]
+  #       attrs.merge!(data[:attributes])
+  #       data = data[:value]
+  #     end
+
+  #     if data.is_a? Hash
+  #       builder.send(node, attrs) do
+  #         data.keys.each do |k|
+  #           builder << to_dom(k, data[k]).root.to_xml
+  #         end
+  #       end
+  #     else
+  #       builder.send(node, data, attrs)
+  #     end
+  #   end
+  #   doc.doc
+  # end
+
+  # def to_xml_body
+  #   doc = self.to_dom('request', self.id_matrix_operation, {:'client-reference'=>self.enquiry[:client_reference], :'reason-for-enquiry'=>self.enquiry[:reason_for_enquiry]}).to_xml.gsub(/(<[\/]?)/,'\1idm:')
+  #   self.xml = doc.gsub('<idm:?xml version="1.0"?>','')
+  # end
+
+  # def id_matrix_operation
+  #   individual_name = {
+  #     :'family-name' => (self.entity[:family_name]).to_s,
+  #     :'first-given-name' => (self.entity[:first_given_name]).to_s,
+  #   }
+  #   individual_name = individual_name.merge(:'other-given-name' => (self.entity[:other_given_name]).to_s) if !self.entity[:other_given_name].blank? #rescue false
+
+  #   date_of_birth = (self.entity[:date_of_birth]) #.strftime("%Y-%m-%d")
+  #   gender = (self.entity[:gender].downcase)
+
+  #   if self.entity[:current_address][:unformatted_address]
+  #      current_address = {:'unformatted-address' => self.entity[:current_address][:unformatted_address]}
+  #   else
+  #     current_address = {
+  #       :'property' => (self.entity[:current_address][:property]),
+  #       :'unit-number' => (self.entity[:current_address][:unit_number]),
+  #       :'street-number' => (self.entity[:current_address][:street_number]),
+  #       :'street-name' => (self.entity[:current_address][:street_name]),
+  #       :'street-type' => (self.entity[:current_address][:street_type]),
+  #       :'suburb' => (self.entity[:current_address][:suburb]),
+  #       :'state' => (self.entity[:current_address][:state]),
+  #       :'postcode' => (self.entity[:current_address][:postcode]),
+  #     }
+  #     current_address.delete(:'unit-number') if self.entity[:current_address][:unit_number].blank? #rescue true
+  #   end
+
+  #   phone = {
+  #     :'numbers' => {
+  #       :'home-phone-number verify="true"' => (self.entity[:home_phone_number]),
+  #       :'mobile-phone-number verify="true"' => (self.entity[:mobile_phone_number])
+  #     }
+  #   }
+
+  #   email_address = (self.entity[:email_address])
+
+  #   medicare_details = {
+  #     :'card-number' => (self.entity[:medicare_card_number]),
+  #     :'reference-number' => (self.entity[:medicare_reference_number]),
+  #     :'card-colour' => (self.entity[:medicare_card_color]),
+  #     :'date-of-expiry' => (self.entity[:medicare_card_expiry])
+  #   }
+
+  #   drivers_licence_details = {
+  #     :'state-code' => (self.entity[:drivers_licence_state_code]),
+  #     :'number' => (self.entity[:drivers_licence_number])
+  #   }
+
+  #   passport_details = {
+  #     :'country-code' => (self.entity[:passport_country_code]),
+  #     :'number' => (self.entity[:passport_number])
+  #   }
+
+  #   # Make sure items generated in order #5519
+  #   details = ActiveSupport::OrderedHash.new
+  #   # details[:'consents'] = consents
+  #   details[:'individual-name'] = individual_name
+  #   details[:'date-of-birth'] = date_of_birth
+  #   details[:'gender'] = gender
+  #   details[:'current-address'] = current_address
+  #   details[:'phone']=  phone
+  #   details[:'email-address'] = email_address
+
+  #   #The search requires that the fields be present to be successful, otherwise we exclude the section
+  #   { 'drivers-licence-details' => drivers_licence_details, 'passport-details' => passport_details, 'medicare' => medicare_details}.each do |section, values|
+  #     details[:"#{section}"] = values unless self.mandatory_values_empty?(values)
+  #   end
+
+  #   details
+  # end
